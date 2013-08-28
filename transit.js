df5c040db3b0c59f2a418f4569c21a86035edc71
Pranav Ravichandran <me@onloop.net>
2013-08-28 17:24:27 +0530

    tabbing issues

--- transit.js
+++ transit.js
@@ -1,79 +1,79 @@
 var transit = (function () {
-	var map = null;
+    var map = null;
 
-	return {
-		initMap : function (coords, mapZoom) {
-			mapZoom = (typeof mapZoom == 'undefined') ? 1 : mapZoom;
-			var mapCenter = new google.maps.LatLng(coords[0], coords[1]);
+    return {
+        initMap : function (coords, mapZoom) {
+            mapZoom = (typeof mapZoom == 'undefined') ? 1 : mapZoom;
+            var mapCenter = new google.maps.LatLng(coords[0], coords[1]);
 
-			var mapDet = {
-				center: mapCenter,
-				zoom: mapZoom,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-			};
+            var mapDet = {
+                center: mapCenter,
+                zoom: mapZoom,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
-			map = new google.maps.Map(document.getElementById('transitMap'), mapDet);
-			return map;
-		},
+            map = new google.maps.Map(document.getElementById('transitMap'), mapDet);
+            return map;
+        },
 
-		initMarker : function (coords, hoverText) {
-			hoverText = (typeof hoverText == 'undefined') ? 'Marker' : hoverText;
-			var markerPos = new google.maps.LatLng(coords[0], coords[1]);
+        initMarker : function (coords, hoverText) {
+            hoverText = (typeof hoverText == 'undefined') ? 'Marker' : hoverText;
+            var markerPos = new google.maps.LatLng(coords[0], coords[1]);
 
-			var markerIcon = {
-				path: google.maps.SymbolPath.CIRCLE,
-				fillColor: 'black',
-				fillOpacity: 0.8,
-				scale: 4
-			};
+            var markerIcon = {
+                path: google.maps.SymbolPath.CIRCLE,
+                fillColor: 'black',
+                fillOpacity: 0.8,
+                scale: 4
+            };
 
-			var marker = {
-				position: markerPos,
-				title: hoverText,
-				icon: markerIcon
-			};
+            var marker = {
+                position: markerPos,
+                title: hoverText,
+                icon: markerIcon
+            };
 
-			marker = new google.maps.Marker(marker);
-			return marker;
-		},
+            marker = new google.maps.Marker(marker);
+            return marker;
+        },
 
-		moveMarkers : function (markerSet, interval) {
-			interval = (typeof interval == 'undefined') ? 1000 : interval;
-			var transition = setInterval(
-					function () {
-						for (var counter = 0; counter < markerSet.length; counter++) {
-							for (var i = 0; i < markerSet.length; i++) {
-								var curPos = new google.maps.LatLng(markerSet[i].coordSet[counter][0],
-									markerSet[i].coordSet[counter][1]);
-								markerSet[i].marker.position = curPos;
-								markerSet[i].marker.setMap(map);
-							}
-						}
-					},
-				    interval);
-		},
+        moveMarkers : function (markerSet, interval) {
+            interval = (typeof interval == 'undefined') ? 1000 : interval;
+            var transition = setInterval(
+                    function () {
+                        for (var counter = 0; counter < markerSet.length; counter++) {
+                            for (var i = 0; i < markerSet.length; i++) {
+                                var curPos = new google.maps.LatLng(markerSet[i].coordSet[counter][0],
+                                    markerSet[i].coordSet[counter][1]);
+                                markerSet[i].marker.position = curPos;
+                                markerSet[i].marker.setMap(map);
+                            }
+                        }
+                    },
+                    interval);
+        },
 
-		constructMarkerSet : function (markers) {
-			var markerSet = new Array();
-			for (var counter = 0; counter < markers.length; counter++) {
-				markerObj = transit.initMarker(markers[counter].coords[0], markers[counter].hoverText);
-				coordList = markers[counter].coords.slice(1);
+        constructMarkerSet : function (markers) {
+            var markerSet = new Array();
+            for (var counter = 0; counter < markers.length; counter++) {
+                markerObj = transit.initMarker(markers[counter].coords[0], markers[counter].hoverText);
+                coordList = markers[counter].coords.slice(1);
 
-				markerSet.push({
-					marker: markerObj,
-					coordSet: coordList
-				});
-			}
-			return markerSet;
-		},
+                markerSet.push({
+                    marker: markerObj,
+                    coordSet: coordList
+                });
+            }
+            return markerSet;
+        },
 
-		init : function (centerCoords, zoom, markerList, interval) {
-			google.maps.event.addDomListener(window, 'load',
-					function () {
-						map = transit.initMap(centerCoords, zoom);
-						var markerSet = transit.constructMarkerSet(markerList);
-						transit.moveMarkers(markerSet, interval);
-					});
-		}
-	};
+        init : function (centerCoords, zoom, markerList, interval) {
+            google.maps.event.addDomListener(window, 'load',
+                    function () {
+                        map = transit.initMap(centerCoords, zoom);
+                        var markerSet = transit.constructMarkerSet(markerList);
+                        transit.moveMarkers(markerSet, interval);
+                    });
+        }
+    };
 })();
